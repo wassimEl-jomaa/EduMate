ef17490c9f6adfcfc119fc7519369099462521cc
wassimEl-jomaa <wassimeljomaa@gmail.com>
2025-04-04 06:13:51 +0200

    create new table for school

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime,timezone
-from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, Date, Text, UniqueConstraint
+from sqlalchemy import Column, DateTime, Integer, String, ForeignKey, Date, Table, Text, UniqueConstraint
 from sqlalchemy.orm import relationship
 from db_setup import Base
 
@@ -16,8 +16,11 @@ class User(Base):
     arskurs_id = Column(Integer, ForeignKey('arskurs.id'), nullable=True)
     role_id = Column(Integer, ForeignKey('role.id'), nullable=True)
     membership_id = Column(Integer, ForeignKey('membership.id'), nullable=True)
-
     membership = relationship("Membership")
+    school_id = Column(Integer, ForeignKey("schools.id"), nullable=True)  # Foreign key to School table
+
+    # Relationships
+    school = relationship("School", back_populates="users")  # Many-to-one relationship
     role = relationship("Role")
     arskurs = relationship("Arskurs")
     tokens = relationship("Token", back_populates="user", cascade="all, delete-orphan")
@@ -33,21 +36,44 @@ class Token(Base):
 
     # Add the relationship to the User model
     user = relationship("User", back_populates="tokens")
-
+# Association table for many-to-many relationship between Arskurs and Teacher
+arskurs_teacher_association = Table(
+    "arskurs_teacher",
+    Base.metadata,
+    Column("arskurs_id", Integer, ForeignKey("arskurs.id", ondelete="CASCADE"), primary_key=True),
+    Column("teacher_id", Integer, ForeignKey("teacher.id", ondelete="CASCADE"), primary_key=True)
+)
 class Teacher(Base):
     __tablename__ = "teacher"
 
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)  # ForeignKey to User table
     subject_id = Column(Integer, ForeignKey("subject.id"), nullable=True)  # Optional: Subject the teacher teaches
+  
     qualifications = Column(Text, nullable=True)  # Teacher's qualifications
     photo = Column(String, nullable=True)
     homeworks = relationship("Homework", back_populates="teacher")  # Use back_populates
-    user = relationship("User", back_populates="teacher")  # Use back_populates instead of backref
+    user = relationship("User", back_populates="teacher")  
     subject = relationship("Subject", backref="teachers")  # Optional: Subject table if you want to associate teachers with subjects
-
+   
+     # Many-to-many relationship with Teacher
+    teachers = relationship("Teacher", secondary=arskurs_teacher_association, back_populates="arskurser")
     def __repr__(self):
         return f"<Teacher(id={self.id}, user_id={self.user_id}, qualifications={self.qualifications})>"
+
+    
+class School(Base):
+    __tablename__ = "schools"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    address = Column(Text, nullable=False)
+    phone_number = Column(String(15), nullable=True)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
+    
+    # Relationship to User
+    users = relationship("User", back_populates="school")  # One-to-many relationship
 class Role(Base):
     __tablename__ = "role"
     
@@ -70,6 +96,8 @@ class Arskurs(Base):
     description = Column(Text, nullable=True)
     skola = Column(String)  # Add skola field
     klass = Column(String)  # Add klass field
+    # Many-to-many relationship with Teacher
+    teachers = relationship("Teacher", secondary=arskurs_teacher_association, back_populates="arskurser")
     __table_args__ = (
         UniqueConstraint("name", "skola", "klass", name="unique_name_skola_klass"),
     )
@@ -140,6 +168,7 @@ class Meddelande(Base):
     message = Column(Text, nullable=False)
     description = Column(Text, nullable=True)  # New column for description
     read_status = Column(String, default="Unread")  # New column for read_status
+    type = Column( nullable=False, default="general")  # New ENUM column
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc))
     homework_id = Column(Integer, ForeignKey("homework.id"))
--- frontend/edumate/src/pages/FeaturesSection.jsx
+++ frontend/edumate/src/pages/FeaturesSection.jsx
@@ -4,24 +4,31 @@ const FeaturesSection = () => {
   return (
     <section id="features" className="py-20 px-6 bg-white">
       {/* Välkommen Sektion */}
-      <div className="text-center mb-12">
-        <h2 className="text-3xl font-semibold text-blue-800 mb-4">
-          Välkommen till vår Plattform!
-        </h2>
-        <p className="text-lg text-gray-700 mb-6">
-          Välkommen till vår innovativa lärplattform! Vi är glada att du har
-          valt att använda vårt system som är designat för att göra utbildning
-          mer effektiv, interaktiv och tillgänglig för både elever och lärare.
-        </p>
-        <p className="text-lg text-gray-700 mb-6">
-          Vår plattform erbjuder en rad funktioner som underlättar planering,
-          bedömning, kommunikation och uppföljning av elevernas framsteg.
-          Oavsett om du är lärare, elev eller förälder, finns det verktyg här
-          för att göra din upplevelse så smidig och engagerande som möjligt.
-        </p>
-        <p className="text-lg text-gray-700">
-          Låt oss hjälpa dig att nå dina mål och ta nästa steg mot framgång!
-        </p>
+      <div className="flex flex-col sm:flex-row items-center mb-12">
+        <div className="sm:w-2/3">
+          <h2 className="text-3xl font-semibold text-blue-800 mb-4">
+            Välkommen till vår Plattform!
+          </h2>
+          <p className="text-lg text-gray-700 mb-6">
+            Välkommen till vår innovativa lärplattform! Vi är glada att du har
+            valt att använda vårt system som är designat för att göra utbildning
+            mer effektiv, interaktiv och tillgänglig för både elever och lärare.
+          </p>
+          <p className="text-lg text-gray-700 mb-6">
+            Vår plattform erbjuder en rad funktioner som underlättar planering,
+            bedömning, kommunikation och uppföljning av elevernas framsteg.
+            Oavsett om du är lärare, elev eller förälder, finns det verktyg här
+            för att göra din upplevelse så smidig och engagerande som möjligt.
+          </p>
+          <p className="text-lg text-gray-700">
+            Låt oss hjälpa dig att nå dina mål och ta nästa steg mot framgång!
+          </p>
+        </div>
+        <img
+          src="grundskolan.jpg"
+          alt="Välkommen"
+          className="sm:w-1/3 w-full h-auto object-cover rounded-md mt-6 sm:mt-0 sm:ml-6"
+        />
       </div>
 
       {/* Funktioner Sektion */}
@@ -57,6 +64,25 @@ const FeaturesSection = () => {
             dina mål.
           </p>
         </div>
+      </div>
+
+      {/* Bild för Funktioner */}
+      <div className="flex flex-col sm:flex-row items-center mt-12">
+        <div className="sm:w-2/3">
+          <h2 className="text-3xl font-semibold text-blue-800 mb-4">
+            Upptäck alla funktioner
+          </h2>
+          <p className="text-lg text-gray-700">
+            Vår plattform är utformad för att möta behoven hos både lärare,
+            elever och föräldrar. Med våra verktyg kan du planera, kommunicera
+            och följa upp på ett enkelt och effektivt sätt.
+          </p>
+        </div>
+        <img
+          src="/Upptäck.jpeg"
+          alt="Funktioner"
+          className="sm:w-1/3 w-full h-auto object-cover rounded-md mt-6 sm:mt-0 sm:ml-6"
+        />
       </div>
     </section>
   );
